02381db6f49830fdf10a80d0ed9d8bb30c041db5
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2020-12-05 14:20:42 -0700

    Add dependent parameter to relationships
    Co-authored-by: Chris Allbritton <63476564+Callbritton@users.noreply.github.com>
    Co-authored-by: Kevin Cuadros <63522369+kevxo@users.noreply.github.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :friendships
-  has_many :friends, :through => :friendships
+  has_many :friendships, dependent: :destroy
+  has_many :friends, through: :friendships
   has_secure_password
 
   validates :name, :email, :password, presence: true
