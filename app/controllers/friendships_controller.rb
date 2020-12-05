a8d0879eff4029834fbd29143199561d917d7387
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2020-12-05 14:22:04 -0700

    Remove needless redirects
    Co-authored-by: Chris Allbritton <63476564+Callbritton@users.noreply.github.com>
    Co-authored-by: Kevin Cuadros <63522369+kevxo@users.noreply.github.com>

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -2,17 +2,15 @@ class FriendshipsController < ApplicationController
   def create
     friend = User.find_by(email: params[:email])
     if friend
-      unless friend.id == current_user.id || current_user.friends.include?(friend)
-        Friendship.create(user_id: current_user.id, friend_id: friend.id)
-        redirect_to '/dashboard'
-      else
+      if friend.id == current_user.id || current_user.friends.include?(friend)
         flash[:error] = "You can't be your own friend" if friend.id == current_user.id
-        flash[:error] = "Already friends with that user" if current_user.friends.include?(friend)
-        redirect_to '/dashboard'
+        flash[:error] = 'Already friends with that user' if current_user.friends.include?(friend)
+      else
+        Friendship.create(user_id: current_user.id, friend_id: friend.id)
       end
     else
-      flash[:error] = "Friend could not be found"
-      redirect_to '/dashboard'
+      flash[:error] = 'Friend could not be found'
     end
+    redirect_to '/dashboard'
   end
 end
