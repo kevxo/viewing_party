62d476d00ddb32ae0df994787969675666930373
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2020-12-05 13:16:54 -0700

    Run migration file
    Co-authored-by: Chris Allbritton <63476564+Callbritton@users.noreply.github.com>
    Co-authored-by: Kevin Cuadros <63522369+kevxo@users.noreply.github.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_01_221500) do
+ActiveRecord::Schema.define(version: 2020_12_05_193707) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "friendships", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "friend_id"
+    t.index ["friend_id"], name: "index_friendships_on_friend_id"
+    t.index ["user_id"], name: "index_friendships_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -23,4 +30,5 @@ ActiveRecord::Schema.define(version: 2020_12_01_221500) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "friendships", "users"
 end
