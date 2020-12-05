4b4876a5568631afd9a3556ca8ac8a43df3cd1b9
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2020-12-05 14:12:11 -0700

    Add tests for index page rendering friends for sad and happy paths
    Co-authored-by: Chris Allbritton <63476564+Callbritton@users.noreply.github.com>
    Co-authored-by: Kevin Cuadros <63522369+kevxo@users.noreply.github.com>

--- spec/features/dashboard/index_spec.rb
+++ spec/features/dashboard/index_spec.rb
@@ -2,19 +2,15 @@ require 'rails_helper'
 
 describe 'as an authenticated user' do
   before (:each) do
-    visit '/registration'
-
-    fill_in :name, with: 'Kevin'
-    fill_in :email, with: 'kev123@example.com'
-    fill_in :password, with: 'passwordsecure'
-    fill_in :password_confirmation, with: 'passwordsecure'
-    click_button 'Register'
+    @user_list = create_list(:user, 5)
+    @first_user = @user_list.first
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@first_user)
   end
   describe 'when I visit /dashboard' do
     it 'I see a welcome message, a button to discover movies, a friends section, and a viewing parties section' do
       visit '/dashboard'
 
-      expect(page).to have_content("Welcome Kevin!")
+      expect(page).to have_content("Welcome #{@first_user.name}!")
       expect(page).to have_button("Discover Movies")
 
       within('.friends') do
@@ -26,6 +22,64 @@ describe 'as an authenticated user' do
       within('.viewing_parties') do
         expect(page).to have_content("Viewing Parties")
       end
+    end
+
+    it 'I can add a friend if that user is a registered user of the application' do
+      visit '/dashboard'
+
+      within('.friends') do
+        fill_in(:email, :with => "#{@user_list.second.email}")
+        click_button("Add Friend")
+      end
+
+      expect(current_path).to eq("/dashboard")
+
+      within('.friends') do
+        expect(page).to have_content("#{@user_list.second.name}")
+      end
+    end
+
+    it 'I see flash message when a user is not a registered member of the application' do
+      visit '/dashboard'
+
+      fake_user = "example@example.com"
+
+      within('.friends') do
+        fill_in(:email, :with => "#{fake_user}")
+        click_button("Add Friend")
+      end
+
+      expect(current_path).to eq("/dashboard")
+
+      expect(page).to have_content("Friend could not be found")
+    end
+
+    it 'I cannot add myself as a friend' do
+      visit '/dashboard'
+
+      within('.friends') do
+        fill_in(:email, :with => "#{@first_user.email}")
+        click_button("Add Friend")
+      end
+
+      expect(current_path).to eq("/dashboard")
+
+      expect(page).to have_content("You can't be your own friend")
+    end
+
+    it 'I cannot add a friend who I am already friends with' do
+      visit '/dashboard'
+
+      Friendship.create!(user_id: @first_user.id, friend_id: @user_list.second.id)
+
+      within('.friends') do
+        fill_in(:email, :with => "#{@user_list.second.email}")
+        click_button("Add Friend")
+      end
+
+      expect(current_path).to eq("/dashboard")
+
+      expect(page).to have_content("Already friends with that user")
     end
   end
 end
