db2c554ad28671b8ec7a56838c054fc310716058
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2020-12-06 16:23:58 -0700

    Create private method for strong params and create guests in create action
    Co-authored-by: Chris Allbritton 
    <63476564+Callbritton@users.noreply.github.com>
    Co-authored-by: Kevin Cuadros <63522369+kevxo@users.noreply.github.com>

--- app/controllers/viewing_party_controller.rb
+++ app/controllers/viewing_party_controller.rb
@@ -2,6 +2,18 @@ class ViewingPartyController < ApplicationController
   def new; end
 
   def create
-    require 'pry' ; binding.pry
+    party = current_user.parties.create(party_params)
+    if params[:guests]
+      params[:guests].values.flatten.each do |id|
+        party.guests.create!(guest_id: id.to_i)
+      end
+    end
+    redirect_to '/dashboard'
+  end
+
+  private
+
+  def party_params
+    params.permit(:movie_title, :duration, :date, :start_time)
   end
 end
