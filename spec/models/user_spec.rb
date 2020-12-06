09b20cf69341d8288c0abf4c657b7e09fadb6da2
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2020-12-06 13:17:53 -0700

    Add relationships to user model for inverse friends
    Co-authored-by: Chris Allbritton <63476564+Callbritton@users.noreply.github.com>
    Co-authored-by: Kevin Cuadros <63522369+kevxo@users.noreply.github.com>

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,6 +14,21 @@ RSpec.describe User, type: :model do
     describe 'relationships' do
       it { should have_many :friendships }
       it { should have_many(:friends).through(:friendships) }
+      it { should have_many :inverse_friendships }
+      it { should have_many(:inverse_friends).through(:inverse_friendships) }
+
+      it 'should have friends and inverse friends' do
+        @current_user = create(:user)
+        @users = create_list(:user, 5)
+
+        @current_user.friendships.create!(friend_id: @users.first.id)
+        @current_user.friendships.create!(friend_id: @users.second.id)
+
+        @users.third.friendships.create!(friend_id: @current_user.id)
+
+        expect(@current_user.friends).to eq([@users.first, @users.second])
+        expect(@current_user.inverse_friends).to eq([@users.third])
+      end
     end
   end
 end
