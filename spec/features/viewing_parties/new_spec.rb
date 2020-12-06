ff48883e21e55f9e2269eb74c0ffe605df753fad
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2020-12-06 16:23:58 -0700

    Add test for rendering parties invited to on dashboard
    Co-authored-by: Chris Allbritton 
    <63476564+Callbritton@users.noreply.github.com>
    Co-authored-by: Kevin Cuadros <63522369+kevxo@users.noreply.github.com>

--- spec/features/viewing_parties/new_spec.rb
+++ spec/features/viewing_parties/new_spec.rb
@@ -1,19 +1,20 @@
 require 'rails_helper'
 
 describe 'As a authenticated user' do
-  describe 'When I visit the new viewing party page' do
+  describe 'When I visit the new viewing party page', :vcr do
     before :each do
       @user = create(:user)
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
 
-      @user_list = create_list(:user, 2)
+      @user_list = create_list(:user, 3)
 
       @user.friendships.create!(friend_id: @user_list.first.id)
-      @user.friendships.create!(friend_id: @user_list.last.id)
+      @user.friendships.create!(friend_id: @user_list.second.id)
+      @user.friendships.create!(friend_id: @user_list.third.id)
     end
 
     it 'should see Movie Title, Duration, Date, Start time,
-    checkboxes to each friend, and create party button', :vcr do
+    checkboxes to each friend, and create party button' do
       visit '/discover'
       click_button("Top 40 Movies")
       expect(current_path).to eq('/movies')
@@ -29,15 +30,16 @@ describe 'As a authenticated user' do
       expect(page).to have_content("Duration of Party")
       expect(page).to have_field(:duration, :with => '142')
 
-      fill_in :day, with: '12/12/2020'
+      fill_in :date, with: '12/12/2020'
 
       expect(page).to have_content('Start Time')
-      fill_in :time,	with: '10:15'
+      fill_in :start_time,	with: '10:15'
 
       expect(page).to have_content('Include')
 
 
       check("#{@user_list.first.name}")
+      check("#{@user_list.second.name}")
 
       click_button 'Create Party'
 
@@ -48,6 +50,47 @@ describe 'As a authenticated user' do
         expect(page).to have_content('12/12/2020')
         expect(page).to have_content('10:15')
         expect(page).to have_content('Hosting')
+      end
+    end
+
+    it 'I should see parties I am invited to' do
+      visit '/discover'
+
+      click_button("Top 40 Movies")
+      expect(current_path).to eq('/movies')
+
+      click_link("The Shawshank Redemption")
+
+      click_button 'Create Viewing Party for Movie'
+
+      expect(current_path).to eq('/viewing_party/new')
+
+      expect(page).to have_content('Viewing Party Details')
+      expect(page).to have_content("Movie title: The Shawshank Redemption")
+      expect(page).to have_content("Duration of Party")
+      expect(page).to have_field(:duration, :with => '142')
+
+      fill_in :date, with: '12/12/2020'
+
+      expect(page).to have_content('Start Time')
+      fill_in :start_time,	with: '10:15'
+
+      expect(page).to have_content('Include')
+
+      check("#{@user_list.first.name}")
+      check("#{@user_list.second.name}")
+
+      click_button 'Create Party'
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user_list.first)
+
+      visit '/dashboard'
+
+      within ".viewing_parties" do
+        expect(page).to have_content('The Shawshank Redemption')
+        expect(page).to have_content('12/12/2020')
+        expect(page).to have_content('10:15')
+        expect(page).to have_content('Invited')
       end
     end
   end
