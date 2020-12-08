7a37effc21f36bfa89f637824810dbcae22f1fba
Chris Allbritton <cla.inbox@gmail.com>
2020-12-07 20:43:14 -0500

    Refactor movies_controller to remove unnecessary code now located in service and facade
    Co-authored-by: Kevin Cuadros <63522369+kevxo@users.noreply.github.com>
    Co-authored-by: Jesse Mellinger <29828129+JesseMellinger@users.noreply.github.com>

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,21 +1,8 @@
 class MoviesController < ApplicationController
   before_action :require_user
   def index
-    if params[:search_title]
-      response = conn.get("/3/search/movie?api_key=#{ENV['MOVIE_API_KEY']}&language=en-US&page=1&include_adult=false&query=#{params[:search_title]}")
-      @movies = JSON.parse(response.body, symbolize_names: true)[:results]
-      if @movies.count == 20
-        response = conn.get("/3/search/movie?api_key=#{ENV['MOVIE_API_KEY']}&language=en-US&page=2&include_adult=false&query=#{params[:search_title]}")
-        @movies << JSON.parse(response.body, symbolize_names: true)[:results]
-      end
-    else
-      page1 = conn.get("/3/movie/top_rated?api_key=#{ENV['MOVIE_API_KEY']}&language=en-US&page=1")
-      page2 = conn.get("/3/movie/top_rated?api_key=#{ENV['MOVIE_API_KEY']}&language=en-US&page=2")
-
-      @movies = []
-      @movies << JSON.parse(page1.body, symbolize_names: true)[:results]
-      @movies << JSON.parse(page2.body, symbolize_names: true)[:results]
-    end
+    search_title = params[:search_title]
+    @movies = MovieFacade.movies(search_title)
   end
 
   def show
@@ -30,10 +17,4 @@ class MoviesController < ApplicationController
     credits = JSON.parse(cast_response.body, symbolize_names: true)
     @cast = credits[:cast]
   end
-end
-
-private
-
-def conn
-  Faraday.new('https://api.themoviedb.org')
 end
