a7828986755030d26888d099d7fb4e98649965fb
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2020-12-08 18:53:10 -0700

    Test for instance method that returns array of genre names
    Co-authored-by: Chris Allbritton 
    <63476564+Callbritton@users.noreply.github.com>
    Co-authored-by: Kevin Cuadros <63522369+kevxo@users.noreply.github.com>

--- spec/poros/movie_spec.rb
+++ spec/poros/movie_spec.rb
@@ -1,22 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Movie do
+  before :each do
+    @attrs = {
+      title: Faker::Movie.title,
+      vote_average: Faker::Number.decimal(l_digits: 1, r_digits: 1).to_s,
+      id: Faker::Number.number(digits: 2),
+      runtime: Faker::Number.number(digits: 3),
+      overview: Faker::Movie.quote,
+      genres: [{:id=>18, :name=>"Drama"}, {:id=>80, :name=>"Crime"}]
+      }
+
+    @movie = Movie.new(@attrs)
+  end
   it "exists" do
-    attrs = {
-      title: "Interstellar",
-      vote_average: "8.8",
-      id: 44,
-      runtime: 233,
-      overview: "It's good."
-    }
+    expect(@movie).to be_a(Movie)
+    expect(@movie.title).to eq(@attrs[:title])
+    expect(@movie.vote_average).to eq(@attrs[:vote_average])
+    expect(@movie.id).to eq(@attrs[:id])
+    expect(@movie.runtime).to eq(@attrs[:runtime])
+    expect(@movie.overview).to eq(@attrs[:overview])
+  end
 
-    movie = Movie.new(attrs)
+  describe '#instance_methods' do
+    it 'genres' do
+      expected = ["Drama", "Crime"]
 
-    expect(movie).to be_a(Movie)
-    expect(movie.title).to eq("Interstellar")
-    expect(movie.vote_average).to eq("8.8")
-    expect(movie.id).to eq(44)
-    expect(movie.runtime).to eq(233)
-    expect(movie.overview).to eq("It's good.")
+      expect(@movie.genres).to eq(expected)
+    end
   end
 end
