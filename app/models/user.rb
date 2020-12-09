9cdf3231f8b0c8f7d984f93281a4bad1bc8c5f0e
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2020-12-08 19:08:05 -0700

    Add dependency relationships
    Co-authored-by: Chris Allbritton 
    <63476564+Callbritton@users.noreply.github.com>
    Co-authored-by: Kevin Cuadros <63522369+kevxo@users.noreply.github.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
   has_many :friendships, dependent: :destroy
   has_many :friends, through: :friendships
-  has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
+  has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id',
+                                 dependent: :destroy, inverse_of: :user
   has_many :inverse_friends, through: :inverse_friendships, source: :user
-  has_many :parties
+  has_many :parties, dependent: :destroy
   has_many :guests, through: :parties
   has_secure_password
 
