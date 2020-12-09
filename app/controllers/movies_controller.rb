e1e1e015be6b18e1703e72d6ecb44a53bece0d98
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2020-12-08 19:09:37 -0700

    Update show action to call movie facade for relevant data
    Co-authored-by: Chris Allbritton 
    <63476564+Callbritton@users.noreply.github.com>
    Co-authored-by: Kevin Cuadros <63522369+kevxo@users.noreply.github.com>

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,15 +6,9 @@ class MoviesController < ApplicationController
   end
 
   def show
-    movie_response = conn.get("/3/movie/#{params[:id]}?api_key=#{ENV['MOVIE_API_KEY']}&language=en-US")
-    @movie = JSON.parse(movie_response.body, symbolize_names: true)
-
-    review_response = conn.get "/3/movie/#{params[:id]}/reviews?api_key=#{ENV['MOVIE_API_KEY']}&language=en-US&page=1"
-    reviews = JSON.parse(review_response.body, symbolize_names: true)
-    @reviews = reviews[:results]
-
-    cast_response = conn.get "/3/movie/#{params[:id]}/credits?api_key=#{ENV['MOVIE_API_KEY']}"
-    credits = JSON.parse(cast_response.body, symbolize_names: true)
-    @cast = credits[:cast]
+    movie_id = params[:id]
+    @movie = MovieFacade.movie(movie_id)
+    @reviews = MovieFacade.reviews(movie_id)
+    @cast = MovieFacade.cast(movie_id)
   end
 end
