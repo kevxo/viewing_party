1c2cf6b902dd0a8bd40b305dde9c602a332a12d2
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2020-12-08 19:06:50 -0700

    Create new class methods to get movie by id, reviews, and cast
    Co-authored-by: Chris Allbritton 
    <63476564+Callbritton@users.noreply.github.com>
    Co-authored-by: Kevin Cuadros <63522369+kevxo@users.noreply.github.com>

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -1,37 +1,55 @@
 class MovieService
-
-  def self.top_40
+  def self.most_popular(limit)
     pages = []
-    page_1 = self.get_top_pages(1)
-    page_2 = self.get_top_pages(2)
-    pages << self.parse_results(page_1)
-    pages << self.parse_results(page_2)
-    pages.flatten!
-  end
-
-  def self.movies_by_title(title)
-    pages = []
-    page_1 = self.get_search_pages(1, title)
-    pages << self.parse_results(page_1)
-    if pages.count == 20
-      page_2 = self.get_search_pages(2, title)
-      pages << self.parse_results(page_2)
+    page = 1
+    until pages.count == limit
+      response = get_top_pages(page)
+      pages << parse_results(response)[:results]
+      pages.flatten!
+      page += 1
     end
-    pages.flatten!
+    pages
   end
 
-  private
+  def self.movies_by_title(title, limit)
+    pages = []
+    page = 1
+    until pages.count == limit
+      response = get_search_pages(page, title)
+      pages << parse_results(response)[:results]
+      pages.flatten!
+      break if pages.count < 20
+
+      page += 1
+    end
+    pages
+  end
+
+  def self.movie_by_id(movie_id)
+    response = conn.get("/3/movie/#{movie_id}?&language=en-US")
+    parse_results(response)
+  end
+
+  def self.get_reviews(movie_id)
+    review_response = conn.get "/3/movie/#{movie_id}/reviews?&language=en-US&page=1"
+    parse_results(review_response)[:results]
+  end
+
+  def self.get_cast(movie_id)
+    cast_response = conn.get "/3/movie/#{movie_id}/credits"
+    parse_results(cast_response)[:cast]
+  end
 
   def self.get_search_pages(page, title)
-    self.conn.get("/3/search/movie?&language=en-US&page=#{page}&include_adult=false&query=#{title}")
+    conn.get("/3/search/movie?&language=en-US&page=#{page}&include_adult=false&query=#{title}")
   end
 
   def self.get_top_pages(page)
-    self.conn.get("/3/movie/top_rated?page=#{page}")
+    conn.get("/3/movie/top_rated?page=#{page}")
   end
 
   def self.parse_results(response)
-    JSON.parse(response.body, symbolize_names: true)[:results]
+    JSON.parse(response.body, symbolize_names: true)
   end
 
   def self.conn
