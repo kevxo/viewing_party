0694904e2802b76063cd95de2cba73f643c34a5c
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2020-12-08 18:58:02 -0700

    Add movie id attribute to party factory
    Co-authored-by: Chris Allbritton 
    <63476564+Callbritton@users.noreply.github.com>
    Co-authored-by: Kevin Cuadros <63522369+kevxo@users.noreply.github.com>

--- spec/factories/parties.rb
+++ spec/factories/parties.rb
@@ -7,5 +7,6 @@ FactoryBot.define do
     duration { Faker::Number.number(digits: 3).to_s }
     date { '12/20/2020' }
     start_time { '10:00' }
+    movie_id { Faker::Number.number(digits: 3).to_s }
   end
 end
