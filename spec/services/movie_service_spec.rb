a376cdb1fdccffdb79d6aa45319490e41c63b11e
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2020-12-08 18:52:10 -0700

    Update test to pass limit to class methods and test additional methods
    Co-authored-by: Chris Allbritton 
    <63476564+Callbritton@users.noreply.github.com>
    Co-authored-by: Kevin Cuadros <63522369+kevxo@users.noreply.github.com>

--- spec/services/movie_service_spec.rb
+++ spec/services/movie_service_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 describe MovieService do
   context "class methods" do
-    context ".top_40" do
+    context ".most_popular" do
       it "returns the top 40 movies", :vcr do
-        movie_service = MovieService.top_40
+        movie_service = MovieService.most_popular(40)
 
         expect(movie_service).to be_a(Array)
         expect(movie_service.first).to be_a(Hash)
@@ -18,9 +18,11 @@ describe MovieService do
         expect(first_result).to have_key(:overview)
         expect(first_result[:overview]).to be_a(String)
       end
+    end
 
+    context ".movies_by_title" do
       it "returns the movies by title", :vcr do
-        movie_service = MovieService.movies_by_title("Interstellar")
+        movie_service = MovieService.movies_by_title("Interstellar", 40)
 
         expect(movie_service).to be_a(Array)
         expect(movie_service.first).to be_a(Hash)
@@ -32,6 +34,49 @@ describe MovieService do
         expect(first_result[:title]).to be_a(String)
         expect(first_result).to have_key(:overview)
         expect(first_result[:overview]).to be_a(String)
+      end
+    end
+
+    context ".movie_by_id" do
+      it "returns the movie by id", :vcr do
+        movie_service = MovieService.movie_by_id("278")
+
+        expect(movie_service).to be_a(Hash)
+        expect(movie_service).to have_key(:title)
+        expect(movie_service).to have_key(:vote_average)
+        expect(movie_service).to have_key(:overview)
+        expect(movie_service[:genres]).to be_a(Array)
+        expect(movie_service[:title]).to eq("The Shawshank Redemption")
+      end
+    end
+
+    context ".get_reviews" do
+      it "returns reviews of movie", :vcr do
+        movie_service = MovieService.get_reviews("278")
+
+        expect(movie_service).to be_a(Array)
+        expect(movie_service.first).to be_a(Hash)
+
+        first_result = movie_service[0]
+        expect(first_result).to have_key(:author)
+        expect(first_result[:author]).to be_a(String)
+        expect(first_result).to have_key(:content)
+        expect(first_result[:content]).to be_a(String)
+      end
+    end
+
+    context ".get_cast" do
+      it "returns cast of movie", :vcr do
+        movie_service = MovieService.get_cast("278")
+
+        expect(movie_service).to be_a(Array)
+        expect(movie_service.first).to be_a(Hash)
+
+        first_result = movie_service[0]
+        expect(first_result).to have_key(:name)
+        expect(first_result[:name]).to be_a(String)
+        expect(first_result).to have_key(:character)
+        expect(first_result[:character]).to be_a(String)
       end
     end
   end
