4c90336d76a73f782f7fcf91923e72c14f1af2e5
Chris Allbritton <cla.inbox@gmail.com>
2020-12-09 20:38:33 -0500

    Add image review cast and movie tests
    Co-authored-by: Kevin Cuadros <63522369+kevxo@users.noreply.github.com>
    Co-authored-by: Jesse Mellinger <29828129+JesseMellinger@users.noreply.github.com>

--- spec/facades/movie_facade_spec.rb
+++ spec/facades/movie_facade_spec.rb
@@ -15,5 +15,41 @@ describe MovieFacade do
         expect(search_facade).to all(be_a(Movie))
       end
     end
+
+    context ".image" do
+      it "returns the top 40 movies and search results", :vcr do
+        image_data = MovieFacade.image(278)
+        expect(image_data).to be_a(Image)
+        expect(image_data.path).to be_a(String)
+      end
+    end
+
+    context ".movie" do
+      it "returns the top 40 movies and search results", :vcr do
+        movie_data = MovieFacade.movie(278)
+        expect(movie_data).to be_a(Movie)
+        expect(movie_data.title).to be_a(String)
+        expect(movie_data.runtime).to be_a(Integer)
+        expect(movie_data.vote_average).to be_a(Float)
+      end
+    end
+
+    context ".reviews" do
+      it "returns the top 40 movies and search results", :vcr do
+        review_data = MovieFacade.reviews(278)
+        expect(review_data).to be_a(Array)
+        expect(review_data.first.author).to be_a(String)
+        expect(review_data.first.content).to be_a(String)
+      end
+    end
+
+    context ".cast" do
+      it "returns the top 40 movies and search results", :vcr do
+        cast_data = MovieFacade.cast(278)
+        expect(cast_data).to be_a(Array)
+        expect(cast_data.first.character).to be_a(String)
+        expect(cast_data.first.name).to be_a(String)
+      end
+    end
   end
 end
